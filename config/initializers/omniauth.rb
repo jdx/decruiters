d423eaf235bb7f52f247a7cf7db5d62be80139d4
dickeyxxx <jeff@dickey.xxx>
2012-11-12 22:48:49 -0800

    trying to fix heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], scope: 'r_fullprofile', fields: %w(id first-name last-name maiden-name formatted-name location picture-url industry num-connections date-of-birth skills)
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], scope: 'r_fullprofile', fields: %w(id first-name last-name maiden-name formatted-name location picture-url industry num-connections date-of-birth skills), :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}
 end
